1415e9df709e7126f9c20caee5ee568e70e53a77
truongluat2201-arch <truongluat2201@gmail.com>
2025-11-13 20:59:14 +0700

    Vidu2
    Ví dụ 2

---
+++ Vidu2.py
@@ -0,0 +1,8 @@
+name="TruongLuat"
+age= 18
+score= 9.3456
+print(f"Tên: {name}, Tuổi: {age}, Điểm: {score}")
+print(f"Điểm làm tròn: {score:.2f}")
+print(f"Điểm phần trăm: {score:.1f}")
+print(f"Tên: {name:<10} | Tuổi: {age:>5}")
+print(f"Số nhị phân: {age:08b}")
