e3332acc87ba65943f4277f431158ae8c60729ea
truongluat2201-arch <truongluat2201@gmail.com>
2025-11-17 08:38:43 +0700

    tinhthoigian

---
+++ tinhthoigian.py
@@ -0,0 +1,5 @@
+gio=int(input("Nhập số giờ: "))
+phut=int(input("Nhập số phút: "))
+giay=int(input("Nhập số giây: "))
+tong_thoi_gian= gio*3600 + phut*60 + giay
+print(f"Tổng thời gian là: {tong_thoi_gian} giây ")
