ca432726082b0df56bb005553215235b85c3b7d2
truongluat2201-arch <truongluat2201@gmail.com>
2025-11-17 07:15:45 +0700

    tinhCV_DThinhchunhat

---
+++ tinhCV_DThinhchunhat.py
@@ -0,0 +1,6 @@
+chieudai=int(input("Nhập chiều dài (m): "))
+chieurong=int(input("Nhập chiều rộng (m): "))
+CV= (chieudai+chieurong)*2
+DT=chieudai*chieurong
+print(f"Chu vi hình chữ nhật là: {CV} m ")
+print(f"Diện tích hình chữ nhật là: {DT} m ")
