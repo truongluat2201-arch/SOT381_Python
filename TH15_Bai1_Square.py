0ef19d61c33b4cc0a8761927eee53bce9d727d33
truongluat2201-arch <truongluat2201@gmail.com>
2025-12-15 13:51:32 +0700

    xong bài

---
+++ TH15_Bai1_Square.py
@@ -0,0 +1,6 @@
+chieu_dai = int(input("Nhập chiều dài: "))
+chieu_rong = int(input("Nhập chiều rộng: "))
+cv = (chieu_dai + chieu_rong)*2
+dt = chieu_dai * chieu_rong
+print(f"Chu vi hình chữ nhật là {cv}")
+print(f"Diện tích hình chữ nhật là {dt}")
