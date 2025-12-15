cf3900648e293eddc4f6b6b17de5b2e572f2c6b0
truongluat2201-arch <truongluat2201@gmail.com>
2025-12-15 14:01:03 +0700

    xong bài

---
+++ TH15_Bai2_IF.py
@@ -0,0 +1,5 @@
+n = int(input("Nhập vào số n: "))
+if (n % 2 == 0) and (n % 3 == 0):
+    print(f"số {n} là số chẳn chia hết cho 3")
+else:
+    print(f"Số {n} không phải là số chẳn chia hết cho 3")
