8064600434af2062c7a68ffdd85d09ef9d799707
truongluat2201-arch <truongluat2201@gmail.com>
2025-12-22 15:19:26 +0700

    xong bài TH 4

---
+++ TH2212_Bai4.py
@@ -0,0 +1,17 @@
+def so_LN_NN(a,b,c):
+    maxx = a
+    if b > maxx:
+        maxx = b
+    if c > maxx:
+        maxx = c
+    minn = a
+    if b < minn:
+        minn = b
+    if c < minn:
+        minn = c
+    return maxx, minn
+
+
+
+x = so_LN_NN(4,5,6)
+print(f"Số lớn nhất và số nhỏ nhất lần lượt là {x}")
