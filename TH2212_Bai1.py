0a222f04d754acde33bb5742ef6024a8b911adb0
truongluat2201-arch <truongluat2201@gmail.com>
2025-12-22 14:27:39 +0700

    sửa bài

--- TH2212_Bai1.py
+++ TH2212_Bai1.py
@@ -1,9 +1,11 @@
-w = float(input("Nhập chiều rông (m): "))
-h = float(input("Nhập chiều dài (m): "))
-if (w <= 0) and (h >= 100):
-    print("Dữ liệu nhập không hợp lệ")
-else:
-    cv = (h + w)*2
-    dt = h*w
+while True:
+    w = float(input("Nhập chiều rông (m): "))
+    h = float(input("Nhập chiều dài (m): "))
+    if (w >= 0.0) and (h <= 100.0):
+        break
+    else:
+        print("Nhập sai dữ liệu")
+cv = (h + w)*2
+dt = h*w
 print(f"Chu vi hình chữ nhật là {cv:.2f} m")
 print(f"Diện tích hình chữ nhật là {dt:.2f} m")
