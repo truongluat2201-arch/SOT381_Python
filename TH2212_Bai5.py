545ec57a4b85be2b37813b36859d582222a1f90b
truongluat2201-arch <truongluat2201@gmail.com>
2025-12-22 15:34:35 +0700

    xong bài 5

---
+++ TH2212_Bai5.py
@@ -0,0 +1,16 @@
+def tinh_s(n):
+    tu_so = 0
+    mau_so = 0
+    # tính tử số
+    for i in range(1,n+1):
+        tu_so = tu_so + i
+    #tính mẫu số
+    for j in range(1,n+1):
+        if j % 2 == 0:
+            mau_so = mau_so + j
+    s = tu_so / mau_so
+    return s
+
+
+x=tinh_s(3)
+print(x)
